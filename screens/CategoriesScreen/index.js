16e759766ae61f3b56ac88cf710f4143d4121760
Augusto Neves <augustoneves@frwk.com.br>
2023-04-08 18:19:15 -0300

    Lessons 90 to 93

---
+++ screens/CategoriesScreen/index.js
@@ -0,0 +1,20 @@
+import { FlatList } from "react-native";
+import { CATEGORIES } from "../../data/data";
+import { CategoryGridTile } from "../../components/CategoryGridTile";
+
+const renderCategoryItem = (itemData) => {
+  return (
+    <CategoryGridTile title={itemData.item.title} color={itemData.item.color} />
+  );
+};
+
+export function CategoriesScreen() {
+  return (
+    <FlatList
+      data={CATEGORIES}
+      renderItem={renderCategoryItem}
+      keyExtractor={(item) => item.id}
+      numColumns={2}
+    />
+  );
+}
